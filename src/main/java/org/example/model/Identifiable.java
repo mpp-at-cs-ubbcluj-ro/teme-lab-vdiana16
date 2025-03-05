425f84758de8e6ced04d9698ec24ecab224c66e6
Pașca Valentina-Diana <dianavalentinapasca@gmail.com>
2025-03-05 17:35:08 +0200

    Delete src directory

--- src/main/java/org/example/model/Identifiable.java
+++
@@ -1,6 +0,0 @@
-package org.example.model;
-
-public interface Identifiable<Tid> {
-    Tid getID();
-    void setID(Tid id);
-}
--- src/main/java/org/example/repository/RequestRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.repository;
-
-import org.example.model.ComputerRepairRequest;
-
-public class RequestRepository extends AbstractRepository<ComputerRepairRequest, Integer>{
-    public RequestRepository(){}
-}
