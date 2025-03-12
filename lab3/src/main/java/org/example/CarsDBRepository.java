b48af5b73f44e1ed2cb8887ea9bbb85b7a66a042
vdiana16 <valentina.pasca@stud.ubbcluj.ro>
2025-03-12 17:44:34 +0200

    Laborator 2

---
+++ lab3/src/main/java/org/example/CarsDBRepository.java
@@ -0,0 +1,100 @@
+package org.example;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class CarsDBRepository implements CarRepository{
+
+    private JdbcUtils dbUtils;
+
+    private static final Logger logger= LogManager.getLogger();
+
+    public CarsDBRepository(Properties props) {
+        logger.info("Initializing CarsDBRepository with properties: {} ",props);
+        dbUtils=new JdbcUtils(props);
+    }
+
+    @Override
+    public List<Car> findByManufacturer(String manufacturerN) {
+        logger.traceEntry("finding cars with manufacturer {}",manufacturerN);
+        Connection con=dbUtils.getConnection();
+        List<Car> cars=new ArrayList<>();
+        try(PreparedStatement preStmt=con.prepareStatement("select * from cars where manufacturer=?")) {
+            preStmt.setString(1, manufacturerN);
+            try (ResultSet result = preStmt.executeQuery()) {
+                while (result.next()) {
+                    int id = result.getInt("id");
+                    String manufacturer = result.getString("manufacturer");
+                    String model = result.getString("model");
+                    int year = result.getInt("year");
+                    Car car = new Car(manufacturer, model, year);
+                    car.setId(id);
+                    cars.add(car);
+                }
+            }
+        } catch (SQLException e) {
+            logger.error(e);
+            System.out.println("Error DB " + e);
+        }
+        logger.traceExit(cars);
+        return cars;
+    }
+
+    @Override
+    public List<Car> findBetweenYears(int min, int max) {
+	//to do
+        return null;
+    }
+
+    @Override
+    public void add(Car elem) {
+        logger.traceEntry("saving car {} ",elem);
+        Connection con=dbUtils.getConnection();
+        try(PreparedStatement preStmt=con.prepareStatement("insert into Cars(manufacturer,model,year) values (?,?,?)")) {
+            preStmt.setString(1, elem.getManufacturer());
+            preStmt.setString(2, elem.getModel());
+            preStmt.setInt(3, elem.getYear());
+            int result = preStmt.executeUpdate();
+            logger.trace("Saved {} instances", result);
+        }catch (SQLException ex){
+            logger.error(ex);
+            System.out.println("Error DB "+ex);
+        }
+        logger.traceExit();
+    }
+
+    @Override
+    public void update(Integer integer, Car elem) {
+      //to do
+    }
+
+    @Override
+    public Iterable<Car> findAll() {
+        logger.traceEntry();
+        Connection con=dbUtils.getConnection();
+        List<Car> cars=new ArrayList<>();
+        try(PreparedStatement preStmt=con.prepareStatement("select * from cars")) {
+            try (ResultSet result = preStmt.executeQuery()) {
+                while (result.next()) {
+                    int id = result.getInt("id");
+                    String manufacturer = result.getString("manufacturer");
+                    String model = result.getString("model");
+                    int year = result.getInt("year");
+                    Car car = new Car(manufacturer, model, year);
+                    car.setId(id);
+                    cars.add(car);
+                }
+            }
+        } catch (SQLException e) {
+            logger.error(e);
+            System.out.println("Error DB " + e);
+        }
+        logger.traceExit(cars);
+        return cars;
+    }
+}
